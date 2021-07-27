e2045ad81db1cdce03121b2e7813ee71db02e2d4
steve-999 <steve.green1@gmail.com>
2021-07-27 17:56:21 +0100

    changed url

--- angular-src/src/environments/environment.ts
+++ angular-src/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  API_BASE_URL: 'http://localhost:5000',
-  //API_BASE_URL: 'https://mean-blog-app-896df.ondigitalocean.app',
+  //API_BASE_URL: 'http://localhost:5000',
+  API_BASE_URL: 'https://mean-blog-app-896df.ondigitalocean.app',
   PORT: 5000,
   MIN_PASSWORD_LENGTH: 1,
   ADMIN_USER_EMAIL: 'admin@gmail.com',
